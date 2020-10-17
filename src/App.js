3bad49499b8ab03e69965c149e0b53ec3a2c174e
Ivan S. Aksenov <aksenov.vanya@yandex.ru>
2020-10-17 08:47:05 +0300

    второй коммит

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Welcome from './Welcome';
+import Secured from './Secured';
+import './App.css';
+
+class App extends Component {
+
+    render() {
+        return (
+            <BrowserRouter>
+                <div className="container">
+                    <ul>
+                        <li><Link to="/">Приветствие</Link></li>
+                        <li><Link to="/secured">Кнопка авторизации (Login)</Link></li>
+                    </ul>
+                    <Route exact path="/" component={Welcome} />
+                    <Route path="/secured" component={Secured} />
+                </div>
+            </BrowserRouter>
+        );
+    }
+}
+export default App;
